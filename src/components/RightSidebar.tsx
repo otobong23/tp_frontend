dbda5d21e9e44625a132dade7524f3d7524c614b
Favour Lawrence <favourlawrence234@gmail.com>
2025-01-02 20:09:37 +0100

    repo updated

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { getUser } from '@/utils/controllers';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 
 const RightSidebar = () => {
