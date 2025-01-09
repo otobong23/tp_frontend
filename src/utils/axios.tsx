7d96248849617b0821ccd84ae7f9a8be8833faf4
Favour Lawrence <favourlawrence234@gmail.com>
2025-01-09 14:24:23 +0100

    repo updated

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -7,7 +7,7 @@ const baseURL = 'https://tradephere-backend.vercel.app/api'
 // Create an Axios instance
 const api = axios.create({
   baseURL,
-  timeout: 1000 * 60, // Optional: Set a timeout (in ms)
+  timeout: 30000, // Optional: Set a timeout (in ms)
   headers: {
     'Content-Type': 'application/json',
   },
