ffe6691d18d3befdb406c887c430287d88c17058
Favour Lawrence <favourlawrence234@gmail.com>
2025-01-10 17:07:23 +0100

    repo updated

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { getUser } from '@/utils/controllers';
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 
